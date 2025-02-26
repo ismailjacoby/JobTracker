49f7512f3ff2e498aeb5e101b5273c43397bf81c
Ismail <ismail.jacoby@gmail.com>
2025-02-26 20:22:20 +0100

    Logout Working & Navbar switches Login and Logout button depending if the user is logged in

--- job-tracker-app/src/app/components/navbar/navbar.component.ts
+++ job-tracker-app/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  constructor(private accountService: AccountService, private router: Router) {}
 
+  isLoggedIn(): boolean {
+    return this.accountService.isLoggedIn();
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
