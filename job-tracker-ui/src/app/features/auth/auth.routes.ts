d18b9ef49e3cb1370cc8816341261085eb3addce
Ismail <ismail.jacoby@gmail.com>
2025-05-16 11:05:27 +0200

    Frontend: Signup component created and Backend implementation

---
+++ job-tracker-ui/src/app/features/auth/auth.routes.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import {SignupComponent} from './pages/signup/signup.component';
+
+export const authRoutes: Routes = [
+  { path: 'signup', component: SignupComponent },
+];
